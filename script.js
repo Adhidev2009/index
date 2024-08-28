5780c58c18ebb221e68cdff018cd927c16acc543
Adhidev2009 <wattswarrior50@gmail.com>
2024-08-28 19:02:37 +0530

    script.js

--- script.js
+++ script.js
@@ -221,3 +221,42 @@ function getIntersection(segment1, segment2) {
 		segment2: ub >= 0 && ub <= 1
 	};
 }
+const gameDiv= document.querySelector('#gameDiv');
+let timer = document.querySelector('#timer');
+let score = document.querySelector('#score');
+
+let startCountDown = setInterval(countDown, 1000);
+
+let timeLeft = 10;
+let totalScore = 0;
+document.addEventListener('DOMContentLoaded', renderGame())
+function renderGame() {
+    const gameCard = document.createElement('div');
+	const click = document.createElement('button');
+    click.textContent = 'CLICK FOR POINTS ';
+    click.addEventListener('click', pointsClick);
+	gameCard.append(click);
+    gameDiv.append(gameCard);
+}
+function pointsClick() {
+    totalScore++
+    score.innerText = totalScore
+}
+function countDown(){
+    timeLeft--;
+    timer.innerText = timeLeft;
+	if(timeLeft === 0){
+        gameOver();
+    }
+}
+function gameOver() {
+    clearInterval(startCountDown);
+
+    const button = document.querySelector('button')
+    button.removeEventListener('click', pointsClick)
+
+    const gameOver = document.createElement('h1');
+    gameOver.innerHTML = 'GAME OVER!' + '<br>' + `Total Score: ${totalScore}`;
+
+    gameDiv.append(gameOver);
+	    }
